91bcab0162b8925b2ebb2d5b6539095c3a33dfcb
Anton Holmquist <anton.holmquist@gmail.com>
2014-10-11 08:14:14 +0200

    Tests

--- app.go
+++ app.go
@@ -9,9 +9,37 @@ import "github.com/franela/goreq"
 
 func main() {
 	fmt.Println("hello world")
+
+	req := goreq.Request {
+		Uri: "http://www.google.com",
+	}
+
+	
+
+	res, err := req.Do()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	
+
+
+	fmt.Printf("%s\n", "\"string\"")
+
+
+	string2, _ := res.Body.ToString()
+	fmt.Printf("%s\n", string2)
+	fmt.Println("test: ", string2)
+
 	app := martini.Classic()
 	app.Get("/", func() string {
-		return "Hello world!"
+
+		
+		
+
+
+		return string2
 	})
 
 	for i := 0; i < 10; i++ {
