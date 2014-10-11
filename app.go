8b650442411785cd9f1ef9dbfe7185c45346c251
Anton Holmquist <anton.holmquist@gmail.com>
2014-10-11 19:06:47 +0200

    Before trying channels

--- app.go
+++ app.go
@@ -3,49 +3,67 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/scanner"
-	"io"
-	"strings"
+	//"text/scanner"
+	//"io"
+	//"strings"
+	"regexp"
 )
 
 import "github.com/go-martini/martini"
 import "github.com/franela/goreq" 
 
+
+
+func run() {
+	htmlReq := goreq.Request {
+		Uri: "https://partner.ikanobank.se/web/FAMILYuppdatera",
+		MaxRedirects: 10,
+	}
+
+	htmlRes, err := htmlReq.Do()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		responseString, _ := htmlRes.Body.ToString()
+
+		re := regexp.MustCompile("<link href=\"")
+
+		var matches [][]int = re.FindAllStringIndex(responseString, -1)
+
+		fmt.Println(matches)
+
+		/*
+		var responseStringScanner scanner.Scanner
+
+		var responseStringReader io.Reader = strings.NewReader(responseString)
+
+
+		responseStringScanner.Init(responseStringReader)
+
+		tok := responseStringScanner.Scan()
+		for tok != scanner.EOF {
+			// do something with tok
+
+
+			tok = responseStringScanner.Scan()
+			fmt.Println("test: ", tok)
+		}
+		*/
+
+		//return responseString
+	}
+}
+
 func main() {
+
+	go run()
 
 	app := martini.Classic()
 
 	app.Get("/", func(res http.ResponseWriter, req *http.Request, params martini.Params) string {
 
-		htmlReq := goreq.Request {
-			Uri: "https://partner.ikanobank.se/web/FAMILYuppdatera",
-			MaxRedirects: 10,
-		}
-
-		htmlRes, err := htmlReq.Do()
-
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			responseString, _ := htmlRes.Body.ToString()
-
-			var responseStringScanner scanner.Scanner
-
-			var responseStringReader io.Reader = strings.NewReader(responseString)
-
-			responseStringScanner.Init(responseStringReader)
-
-			tok := responseStringScanner.Scan()
-			for tok != scanner.EOF {
-				// do something with tok
-
-
-				tok = responseStringScanner.Scan()
-				fmt.Println("test: ", tok)
-			}
-
-			return responseString
-		}
+		
 
 		//https://partner.ikanobank.se/web/FAMILYuppdatera
 
