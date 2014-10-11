53353deefae444aba92bce2f7b04c55c7d7a1cef
Anton Holmquist <anton.holmquist@gmail.com>
2014-10-11 20:19:23 +0200

    Managed to replace

--- app.go
+++ app.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	//"text/scanner"
 	//"io"
-	//"strings"
+	"strings"
 	"regexp"
 )
 
@@ -23,15 +23,36 @@ func fetch(completion chan string) {
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
+
 		responseString, _ := htmlRes.Body.ToString()
 
-		re := regexp.MustCompile("<link href=\"")
+		re := regexp.MustCompile("<link href=\"/web/")
 
 		var matches [][]int = re.FindAllStringIndex(responseString, -1)
 
+        modifiedResponseString := responseString
+
+        for i := 0; i < len(matches); i++ {
+
+            match := matches[i];
+            from := match[0]
+            to := match[1]
+
+            oldString := responseString[from:to + 38]
+            newString := "<link href=\"https://partner.ikanobank.se/web/"
+
+            modifiedResponseString = strings.Replace(modifiedResponseString, oldString, newString, -1)
+
+            fmt.Println(from)
+            fmt.Println(to)
+            fmt.Println(oldString)
+            fmt.Println(newString)
+
+        }
+
 		fmt.Println(matches)
 
-		completion <- responseString
+		completion <- modifiedResponseString
 
 	}
 }
